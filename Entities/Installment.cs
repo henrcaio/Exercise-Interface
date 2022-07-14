09230e63a7865f54875ed78b311f68511e50c169
henrcaio <caioh96@gmail.com>
2022-07-14 00:21:59 -0300

    first commit

---
+++ Entities/Installment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_Interface.Entities {
+    class Installment {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount) {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+        public override string ToString() {
+            return DueDate.ToString("d")
+                + " - "
+                + Amount.ToString("F2");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Exercise_Interface.Entities;
+using Exercise_Interface.Services;
+
+namespace Exercise_Interface {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter contract data: ");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine());
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract myContract = new Contract(contractNumber, contractDate, contractValue);
+            ContractService contractService = new ContractService(new PaypalService());
+
+            contractService.ProcessContract(myContract, months);
+            
+            Console.Clear();
+            Console.WriteLine("Installments: ");
+            foreach (var installment in myContract.Installments) {
+                Console.WriteLine(installment);
+            }
+
+
+        }
+    }
+}
